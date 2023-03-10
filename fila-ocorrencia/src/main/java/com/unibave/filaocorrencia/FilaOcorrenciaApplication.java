56a8d3ee8bb418e9946addb77bd70048d3b8e8d8
Sheila Schlickmann <sheila31sl@hotmail.com>
2023-03-09 22:55:21 -0300

    Criação da estrutura do projeto ocorrência

---
+++ fila-ocorrencia/src/main/java/com/unibave/filaocorrencia/FilaOcorrenciaApplication.java
@@ -0,0 +1,13 @@
+package com.unibave.filaocorrencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilaOcorrenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilaOcorrenciaApplication.class, args);
+	}
+
+}
---
+++ fila-ocorrencia/src/test/java/com/unibave/filaocorrencia/FilaOcorrenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unibave.filaocorrencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilaOcorrenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
